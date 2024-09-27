5d0e9002758b33982c5f349830dad2c5ddc7a0c9
Will Ocanto <ocantowilliam90@gmail.com>
2024-09-27 16:40:46 -0300

    initial commit

---
+++ src/main/java/com/mindHub/waveCenter/WaveCenterApplication.java
@@ -0,0 +1,23 @@
+package com.mindHub.waveCenter;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class WaveCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaveCenterApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner initData(){
+
+		return args -> {
+
+			System.out.println("Hello");
+		};
+	}
+}
---
+++ src/test/java/com/mindHub/waveCenter/WaveCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindHub.waveCenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaveCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

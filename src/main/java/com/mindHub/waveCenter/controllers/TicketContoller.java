0654219be7d46cf251fc14acec81798a19281c14
LudwingValecillos <ludwingvaldev@gmail.com>
2024-09-30 13:31:28 -0300

    a

--- src/main/java/com/mindHub/waveCenter/controllers/TicketContoller.java
+++ src/main/java/com/mindHub/waveCenter/controllers/TicketContoller.java
@@ -1,13 +1,11 @@
 package com.mindHub.waveCenter.controllers;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/stand")
-public class StandController {
-
+@RequestMapping("/api/ticket")
+public class TicketContoller {
 
 
 }

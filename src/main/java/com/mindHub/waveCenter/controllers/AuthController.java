7faa438585d10fd88e77d1193917fc2d5f6aad8f
LudwingValecillos <ludwingvaldev@gmail.com>
2024-09-30 00:32:37 -0300

    a

--- src/main/java/com/mindHub/waveCenter/controllers/AuthController.java
+++ src/main/java/com/mindHub/waveCenter/controllers/AuthController.java
@@ -8,13 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RestController("/api/auth")
+@RequestMapping("/api/auth")
 public class AuthController {
 
     @Autowired
